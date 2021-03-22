237a2e968db9c1602ffbf4b200b10771be62fdc2
“杜科” <duke@zhihu.com>
2021-03-22 19:37:24 +0800

    init

--- app/src/main/java/com/test/ocr_test/MainActivity.kt
+++ app/src/main/java/com/test/ocr_test/MainActivity.kt
@@ -1,18 +1,20 @@
 package com.test.ocr_test
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.test.ocrlib.CaptureActivity
 import com.test.ocrlib.FileCopyHelper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startActivity(Intent(this, CaptureActivity::class.java))
     }
 
     override fun onResume() {
         super.onResume()
-        Toast.makeText(this, "${FileCopyHelper.copyChiToSDCard(this)}", Toast.LENGTH_SHORT).show()
     }
 }
