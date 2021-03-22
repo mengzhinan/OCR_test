dbe364e337213f5d2b3126050b468873b0e1207a
duke <136239173@qq.com>
2021-03-22 23:11:54 +0800

    init

--- app/src/main/java/com/test/ocr_test/MainActivity.kt
+++ app/src/main/java/com/test/ocr_test/MainActivity.kt
@@ -1,20 +1,16 @@
 package com.test.ocr_test
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.test.ocrlib.CaptureActivity
-import com.test.ocrlib.FileCopyHelper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        startActivity(Intent(this, CaptureActivity::class.java))
-    }
 
-    override fun onResume() {
-        super.onResume()
+        startActivity(Intent(this, CaptureActivity::class.java))
+
     }
 }
